94e1532402808d2dba4e284242cf62043e30988d
Matt Powell <matt@mpowell.tech>
2021-08-12 17:12:45 +1000

    Removed unneeded parameters from Bit by bit link in footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 <!-- footer Section Starts Here -->
 <section class="footer">
     <div class="container text-center">
-        <p>All rights reserved. Designed By <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">Bit by bit</a></p>
+        <p>All rights reserved. Designed By <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Bit by bit</a></p>
     </div>
 </section>
 <!-- footer Section Ends Here -->
