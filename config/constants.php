dfaa8e40754e0e3b8012aad2cc0bdc9600a35739
Benjamin Estrade <benegraphicdesign@gmail.com>
2021-08-18 22:31:30 +1000

    Updating constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,15 +1,16 @@
 <?php
-//Start Session
-session_start();
+    //Start Session
+    session_start();
 
 
-//Create Constants to Store Non Repeating Values
-define('SITEURL', 'https://brisbanepetrescue.me/');
-define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'food-order');
+    //Create Constants to Store Non Repeating Values
+    define('SITEURL', 'https://brisbanepetrescue.me/');
+    define('LOCALHOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', 'PL6VCaRJ978WB4');
+    define('DB_NAME', 'pet_rescue_db');
 
-// TODO - Get SQL working (needs DB created first)
-//$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
-//$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
+    // TODO - Get SQL working (needs DB created first)
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
+    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
+?>
