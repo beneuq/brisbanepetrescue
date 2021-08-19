4d69c7d10291d37d9a8623d1263c2f19dffb73ed
Matt Powell <matt@mpowell.tech>
2021-08-19 18:26:03 +1000

    Database connection is working, added skip param

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,9 @@
     //Start Session
     session_start();
 
+    // Set to true if running test server locally, some features will not work.
+    const SKIP_DB_CONNECTION = false;
+
     //Create Constants to Store Non Repeating Values
     const SITEURL = 'https://brisbanepetrescue.me/';
     const LOCALHOST = 'localhost';
@@ -9,10 +12,10 @@
     const DB_PASSWORD = 'PL6VCaRJ978WB4';
     const DB_NAME = 'pet_rescue_db';
 
-    // TODO - Get SQL working
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME); //Database Connection
-    if (!$conn) {
-        die("Could not connect to db" . mysqli_connect_error());
+    if (!SKIP_DB_CONNECTION) {
+        $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME); //Database Connection
+        if (!$conn) {
+            die("Could not connect to db" . mysqli_connect_error());
+        }
     }
-    echo "Connected successfully";
 ?>
