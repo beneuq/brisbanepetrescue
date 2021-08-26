1f2fee708bdd8322e559079ccb0790629ca0b4b7
Matt Powell <matt@mpowell.tech>
2021-08-26 20:12:22 +1000

    Added dogs table page

--- dogs.php
+++ dogs.php
@@ -1,43 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <!-- import generic head section -->
-        <?php 
-        $page_title = 'Breeds';
-        include('partials/head.php'); 
-        $active_dogs = 'active';
+<head>
+    <!-- import generic head section -->
+    <?php
+    $page_title = 'Dogs';
+    include('partials/head.php');
+    $active_help = 'active';
+    ?>
+</head>
+
+<body>
+<div class="underneath-nav"></div>
+<!-- import menu -->
+<?php include('partials/menu.php'); ?>
+
+<!-- Start main page body -->
+<h1 class="breed-title">Dogs looking for a home</h1>
+
+<!-- This code iterates through the database and adds a table row for each breed in the database -->
+<div id="sqldata">
+    <table class="breeds-table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Breed</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Shelter</th>
+            <th>Image</th>
+        </tr>
+        <thead>
+        <tbody>
+        <?php
+        $res = mysqli_query($conn,"
+            SELECT dogs.name as Dog, Breed, age, gender, shelters.name as Shelter, path
+            FROM dogs 
+            INNER JOIN shelters ON dogs.shelter_id = shelters.shelter_id
+            INNER JOIN dog_breeds ON dogs.breed_id = dog_breeds.breed_id
+            INNER JOIN breed_image ON dogs.breed_id = breed_image.breed_id
+            WHERE owner_id IS NULL
+            AND main_image
+            ORDER BY dogs.name
+        ");
+        while($entry = mysqli_fetch_array($res)) {
+            echo "<tr>
+                                <td style='font-weight:bold; width:20%;'>" . $entry['Dog'] . "</td>
+                                <td style='width:20%;'>" . $entry['Breed'] . "</td>
+                                <td style='width:10%;'>" . $entry['age'] . " years</td>
+                                <td style='width:10%;'> <img src='/images/icons/". $entry['gender'] .".png' alt='dog image' width='20%'> </td>
+                                <td style='width:20%;'>" . $entry['Shelter'] . "</td>
+                                <td style='width:20%;'> <img src='". SITEURL.$entry['path'] ."' alt='dog image' width='50%'> </td>
+                            </tr>";
+        }
+        mysqli_close($conn)
         ?>
-    </head>
+        <tbody>
+    </table>
+</div>
+<!-- End main page body -->
 
-    <body>
-        <div class="underneath-nav"></div>
-        <!-- import menu -->
-        <?php include('partials/menu.php'); ?>
+<!-- import footer -->
+<?php include('partials/footer.php'); ?>
+</body>
 
-        <!-- Hero Section -->
-        <section class="hero-section pos-relative">
-            <div class="flex container">
-                <div class="flex f-col hero-content">
-                    <h1 class="pad-bottom-1"><?php echo $page_title; ?></h1>
-                </div>
-            </div>
-        </section>
-        <!-- Section 2 -->
-        <section class="pg-section">
-
-        </section>
-        <!-- Section 3 -->
-        <section class="pg-section" style="background-color: grey;">
-            
-        </section>
-        <!-- Section 4 -->
-        <section class="pg-section">
-            
-        </section>
-
-        <!-- FOOTER -->
-        <?php include('partials/footer.php'); ?>
-    </body>
-    
 </html>
