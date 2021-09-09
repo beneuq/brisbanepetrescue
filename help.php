5cee3849f1fb42f279a2b7cb6ea42f1e76356aa4
rosario-s <rosario_spata@outlook.com>
2021-09-09 14:27:08 +1000

    Revert "Merge branch 'main' of https://github.com/beneuq/brisbanepetrescue into main"
    This reverts commit 5bfb007ab927b8c22b4058c757dc58b963ae7b88, reversing
    changes made to 2a03f2ce0387aba497d16ba68119025b05fa9dcc.

--- help.php
+++ help.php
@@ -10,43 +10,25 @@
         ?>
     </head>
 
-    <body class="help-main">
+    <body>
         <div class="underneath-nav"></div>
         <!-- import menu -->
         <?php include('partials/menu.php'); ?>
 
         <!-- Hero Section -->
-        <section class="hero-section about-hero pos-relative">
+        <section class="hero-section small-hero pos-relative">
             <div class="flex container">
                 <div class="flex f-col hero-content">
-                    <h1 class="about-title"><?php echo $page_title; ?></h1>
+                    <h1 class="pad-top-2 pad-bottom-1"><?php echo $page_title; ?></h1>
+                    <p class="pad-bottom-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat id earum iste minus sed libero alias nam illo aut nemo deserunt, temporibus necessitatibus ad, magnam est facere? Cumque adipisci quis aliquam.</p>
+
                 </div>
             </div>
         </section>
         <!-- Section 2 -->
         <section class="pg-section container">
             <a href="breeds.php"><h2>Learn more about Breeds</h2></a>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat id earum iste minus sed libero alias nam illo aut nemo deserunt, temporibus necessitatibus ad, magnam est facere? Cumque adipisci quis aliquam.</p>
-
-            <div class="help-box">
-                <div class="help-content">
-                    <h2>Get help with your dog</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat id earum iste minus sed libero alias nam illo aut nemo deserunt, temporibus necessitatibus ad, magnam est facere?</p>
-                    <a href="">Read More</a>
-                </div>
-                <div class="help-thumbnail">
-                </div>
-            </div>
-
-            <div class="help-box">
-                <div class="help-content">
-                    <h2>Should I be doing this for my dog?</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat id earum iste minus sed libero alias nam illo aut nemo deserunt.</p>
-                    <a href="">Read More</a>
-                </div>
-                <div class="help-thumbnail">
-                </div>
-            </div>
+            
         </section>
 
         <!-- FOOTER -->
