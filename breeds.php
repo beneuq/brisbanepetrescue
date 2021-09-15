ef384bc668290e2562f9eb788547221eacdac04a
Benjamin Estrade <benegraphicdesign@gmail.com>
2021-09-15 21:32:22 +1000

    adding filter section to breeds page

--- breeds.php
+++ breeds.php
@@ -13,10 +13,19 @@
 <body>
     <div class="underneath-nav"></div>
     <!-- import menu -->
-    <?php include('partials/menu.php');?>
+    <?php include('partials/menu.php'); ?>
 
     <!-- Start main page body -->
     <h1 class="table-title">Dog Information by Breed</h1>
+
+    <!-- Adding the filter section -->
+    <div id="filters">
+        <?php
+        $filterTable = "breed_filters";
+        $table = "dog_breeds d";
+        $page = "breeds";
+        include('partials/filter-menu.php') ?>
+    </div>
 
     <div id="sqldata">
         <table class="breeds-table">
@@ -29,13 +38,13 @@
                     <th>Popularity</th>
                     <th class="text-left">Size Class</th>
                 </tr>
-            <thead>
-            <tbody>    
-                <?php
-                $user_id_or_null = logged_in() ? get_userid() : "NULL"; //todo deal with any vs null (favourite the Affenpinscher signed in as mpowell to see)
-                $user_id_or_any = logged_in() ? get_userid() : "'%'"; //todo deal with any vs null
-                // Gets all breeds, and also whether the breed has been favourited by the logged-in user
-                $res = mysqli_query($conn, "
+                <thead>
+                <tbody>
+                    <?php
+                    $user_id_or_null = logged_in() ? get_userid() : "NULL"; //todo deal with any vs null (favourite the Affenpinscher signed in as mpowell to see)
+                    $user_id_or_any = logged_in() ? get_userid() : "'%'"; //todo deal with any vs null
+                    // Gets all breeds, and also whether the breed has been favourited by the logged-in user
+                    $res = mysqli_query($conn, "
                     SELECT DISTINCT
                         d.breed_id,
                         Breed,
@@ -46,43 +55,39 @@
                         IF(user_id={$user_id_or_null}, 'full', 'empty') as favourite_icon
                         FROM dog_breeds d
                             LEFT JOIN favourite_breeds f ON d.breed_id = f.breed_id
-                        WHERE (user_id LIKE {$user_id_or_any} OR user_id IS NULL OR (user_id LIKE '%' AND user_id NOT LIKE {$user_id_or_any}))
-                        ORDER BY Breed
+                        WHERE (user_id LIKE {$user_id_or_any} OR user_id IS NULL OR (user_id LIKE '%' AND user_id NOT LIKE {$user_id_or_any})) $whereFilters
+                        ORDER BY $orderFilter
                 ");
-                while($entry = mysqli_fetch_array($res)) {
-                    $size_image = "images/icons/dog_size_" . $entry['size_class'];
+                    while ($entry = mysqli_fetch_array($res)) {
+                        $size_image = "images/icons/dog_size_" . $entry['size_class'];
                     ?>
-                    <!-- Start Individual Breed Row -->
-                    <tr id='breed_id=<?php echo $entry['breed_id'];?>'>
-                        <td style='width:10%;'>
-                            <form method='POST' action='/form_submissions/favourite_breed.php'>
-                                <button type='submit' name='breed_id' value='<?php echo $entry['breed_id'];?>'>
-                                    <img width='20%' src='images/icons/heart-<?php echo $entry['favourite_icon'];?>.png'
-                                        onmouseover='favHover(this,"<?php echo $entry['favourite_icon'];?>");'
-                                        onmouseout='favUnhover(this,"<?php echo $entry['favourite_icon'];?>");'
-                                         class="zoom-on-hover"
-                                    >
-                                </button>
-                            </form>
-                        </td>
-                        <td style='width:20%;' class='breed-name'><a href='category-breeds.php?breed_id=<?php echo $entry['breed_id'];?>'><?php echo $entry['Breed'];?></a></td>
-                        <td style='width:22%;'><?php echo $entry['intelligence_desc'];?></td>
-                        <td style='width:16%;' class='text-center'><?php echo str_repeat(EMOJI_DOLLAR,$entry['lifetime_cost_class']);?></td>
-                        <td style='width:10%;' class='text-left'><?php echo str_repeat(EMOJI_STAR,$entry['popularity_class']);?></td>
-                        <td style='width:16%;'><img src='images/icons/dog_size_<?php echo $entry['size_class'];?>' alt='dog size chart' width='50%'></td>
-                    </tr>
-                    <!-- End Individual Breed Row -->
-                <?php
+                        <!-- Start Individual Breed Row -->
+                        <tr id='breed_id=<?php echo $entry['breed_id']; ?>'>
+                            <td style='width:10%;'>
+                                <form method='POST' action='/form_submissions/favourite_breed.php'>
+                                    <button type='submit' name='breed_id' value='<?php echo $entry['breed_id']; ?>'>
+                                        <img width='20%' src='images/icons/heart-<?php echo $entry['favourite_icon']; ?>.png' onmouseover='favHover(this,"<?php echo $entry['favourite_icon']; ?>");' onmouseout='favUnhover(this,"<?php echo $entry['favourite_icon']; ?>");' class="zoom-on-hover">
+                                    </button>
+                                </form>
+                            </td>
+                            <td style='width:20%;' class='breed-name'><a href='category-breeds.php?breed_id=<?php echo $entry['breed_id']; ?>'><?php echo $entry['Breed']; ?></a></td>
+                            <td style='width:22%;'><?php echo $entry['intelligence_desc']; ?></td>
+                            <td style='width:16%;' class='text-center'><?php echo str_repeat(EMOJI_DOLLAR, $entry['lifetime_cost_class']); ?></td>
+                            <td style='width:10%;' class='text-left'><?php echo str_repeat(EMOJI_STAR, $entry['popularity_class']); ?></td>
+                            <td style='width:16%;'><img src='images/icons/dog_size_<?php echo $entry['size_class']; ?>' alt='dog size chart' width='50%'></td>
+                        </tr>
+                        <!-- End Individual Breed Row -->
+                    <?php
                     }
                     mysqli_close($conn)
-                ?>
+                    ?>
             <tbody>
         </table>
     </div>
     <!-- End main page body -->
 
     <!-- import footer -->
-    <?php include('partials/footer.php');?>
+    <?php include('partials/footer.php'); ?>
 </body>
 
 </html>
