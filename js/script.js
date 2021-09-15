d7feb384f6209738a5623181d8dbbfd57e06f13a
Sean <smarshallean@gmail.com>
2021-09-15 22:47:32 +1000

    Adusted the retractable navbar code

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@ $(document).ready(function(){
     window.onscroll = function () {
 
         const currentScrollpos = window.pageYOffset;
-        if (prevScrollpos > currentScrollpos) {
+        if (currentScrollpos >= 100) {
+            if (prevScrollpos > currentScrollpos) {
                 document.getElementById("main-menu").style.top = "0";
             } else {
                 document.getElementById("main-menu").style.top = "-130px";
             }
-
+        } 
+       
             prevScrollpos = currentScrollpos;
         }
 
