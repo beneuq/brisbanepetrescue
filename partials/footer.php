3f188ed1f11713969bbf0ce7a7933ebfe9c03434
Sean <smarshallean@gmail.com>
2021-09-16 00:01:42 +1000

    Added more content to footer. Should be fully responsive as well.

--- partials/footer.php
+++ partials/footer.php
@@ -1,8 +1,28 @@
 <!-- footer Section Starts Here -->
 <footer>
-    <div class="container">
+    <!-- <div class="container">
         <p>©2021 Brisbane Pet Rescue. Designed by 'Bit By Bit'</p>
-    </div>
+    </div> -->
+    <footer id="page-footer" class="pad-top-2 pad-bottom-2">
+        <div class="container">
+            <h3 class="pad-bottom-2">Brisbane Pet Rescue</h3>
+
+            <!-- Footer menu -->
+            <nav>
+                <ul class="flex sm-column">
+                    <li><a href="/index.php">Home</a></li>
+                    <li><a href="/dogs.php">Dogs</a></li>
+                    <li><a href="/breeds.php">Breeds</a></li>
+                    <li><a href="/about-us.php">About Us</a></li>
+                    <li><a href="/help.php">Pet Help</a></li>
+                    <li><a href="#">Contact</a></li>
+
+                </ul>
+            </nav>
+
+            <p class="pad-top">©2021 Brisbane Pet Rescue. Designed by 'Bit By Bit'</p>
+        </div>
+
 </footer>
 <script src="/js/jquery-3.5.1.min.js"></script>
 <script src="/js/script.js" async defer></script>
