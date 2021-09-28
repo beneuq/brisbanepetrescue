3c0ff2a0195df5185c54931de2977469ef2aff1b
SailorCosmos <s.guarrera@uqconnect.edu.au>
2021-09-28 16:45:17 +1000

    Made some changes to the algorithm
    Pretty much the title. Uploading for testing as it should work. Might have to take it down and make some more changes tho

--- js/quiz.js
+++ js/quiz.js
@@ -16,7 +16,7 @@ for (let i = 0; i < answerSliders.length; i++) {
     answerSliders[i].value = 3;
     // Different default for toggle button
     if (answerSliders[i].classList.contains("quizToggle")) {
-        answerSliders[i].step = 4;
+        answerSliders[i].step = 5;
         answerSliders[i].value = 1;
     }
     // Assign function to update scores if slider is moved
@@ -69,24 +69,33 @@ function updateScores(slider, answerNA) {
             // First check if NA is currently ticked
             const inverted_slider =  slider.classList.contains("quizInvert");
             if (currentNA == false) {
-                // Whether the slider is inverted to the actual sql data (5<->1, 4<->2)
-                if (inverted_slider) { // If the slider is inverted to the actual sql data (5<->1, 4<->2)
-                    breed_scores[breed_id] += Math.abs(answer - results[breed_id]) + 1;
-                } else {
-                    breed_scores[breed_id] += (5 - Math.abs(answer - results[breed_id]));
-                }
+                // Call the function that takes care of the calculations
+                breed_scores[breed_id] += getScores(question, breed_id, answer, results);
+
+                // // Whether the slider is inverted to the actual sql data (5<->1, 4<->2)
+                // if (inverted_slider) { // If the slider is inverted to the actual sql data (5<->1, 4<->2)
+                //     // breed_scores[breed_id] += Math.abs(answer - results[breed_id]) + 1;
+                    
+                // } else {
+                //     // breed_scores[breed_id] += (5 - Math.abs(answer - results[breed_id]));
+                // }
             }   
 
             // Check if NA was only recently ticked cause if it was, we need to remove previous scores
             if (previousNA == false) {
                 // Also revert the score changes made by the last selected answer
-                if (previousAnswer) { //todo remove false
-                    if (inverted_slider) { // If the slider is inverted to the actual sql data (5<->1, 4<->2)
-                        breed_scores[breed_id] -= (Math.abs(previousAnswer - results[breed_id]) + 1);
-                    } else {
-                        breed_scores[breed_id] -= (5 - Math.abs(previousAnswer - results[breed_id]));
-                    }
-                }
+                // Call the function that takes care of the calculations
+                breed_scores[breed_id] -= getScores(question, breed_id, previousAnswer, results);
+
+                // if (previousAnswer) { //todo remove false
+                //     if (inverted_slider) { // If the slider is inverted to the actual sql data (5<->1, 4<->2)
+                //         // breed_scores[breed_id] -= (Math.abs(previousAnswer - results[breed_id]) + 1);
+                        
+
+                //     } else {
+                //         // breed_scores[breed_id] -= (5 - Math.abs(previousAnswer - results[breed_id]));
+                //     }
+                // }
             }
             // Update the cookie
             setCookie("breed_scores", JSON.stringify(breed_scores), 1);
@@ -103,6 +112,49 @@ function updateScores(slider, answerNA) {
     };
     xmlReq.open("get", "/form_submissions/return_breed_info.php?lookup_field="+question, true);
     xmlReq.send();
+}
+
+// This function will take the variables and perform different weights based on the information
+function getScores(question, breed_id, answer, results) {
+    // A switch statement will be used to check what question we are dealing with
+    switch(question) {
+        case "size_class":
+            // The size class is valued as an important attiribute
+            return (10 - Math.abs((answer * 2) - results[breed_id]));
+        case "average_purchase_price_class":
+            // Question is valued as important
+            return (10 - Math.abs((answer * 2) - results[breed_id]));
+        case "lifetime_cost_class":
+            // Question is valued as important
+             return (10 - Math.abs((answer * 2) - results[breed_id]));
+        case "watchdog_class":
+            // Question is not valued as important
+            return (5 - Math.abs(answer - results[breed_id]));
+        case "tolerates_being_alone_class":
+            // Question is not valued as important
+            return (5 - Math.abs(answer - results[breed_id]));
+        case "potential_for_playfulness_class":
+            // Question is not valued as important
+            return (5 - Math.abs(answer - results[breed_id]));
+        case "incredibly_kid_friendly_class":
+            // Question is valued as important
+            return (10 - Math.abs((answer * 2) - results[breed_id]));
+        case "good_for_novice_owners":
+            // Question is valued as important. Question also inverted
+            return (Math.abs((answer * 2) - results[breed_id]) + 1);
+        case "exercise_needs_class":
+            // Question is not valued as important
+            return (5 - Math.abs(answer - results[breed_id]));
+        case "apartment_living_class":
+            // Question is valued as important and also question inverted
+            return (Math.abs((answer * 2) - results[breed_id]) + 1);
+        case "dog_friendly_class":
+            // Question is not valued as important
+            return (5 - Math.abs(answer - results[breed_id]));
+        default:
+            // This should not execute
+            console.log("Something went wrong with scoring!!");
+    }
 }
 
 function updateBreedResults() {
