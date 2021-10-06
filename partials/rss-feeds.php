94851b08dfaf2293fa513e65366e1bf2a655ffb0
Matt Powell <matt@mpowell.tech>
2021-10-06 13:13:41 +1000

    Fixed RSS feeds

--- partials/rss-feeds.php
+++ partials/rss-feeds.php
@@ -2,10 +2,11 @@
 
 <section class="pg-section">
     <h2 class="center-txt">Some Pet Care Articles from our Partners</h2>
-    <div class="task-set rss-feed rss-left">
+    <div class="rss-feed rss-left">
         <script src="//rss.bloople.net/?url=https%3A%2F%2Fwww.doggroomingcoursesonline.com%2Fblog%2Ffeed%2F&detail=100&limit=<?php echo RSS_ITEMS_TO_DISPLAY ?>&showicon=false&type=js"></script>
     </div>
-    <div class="task-set rss-feed rss-right">
+    <div class="rss-feed rss-right">
         <script src="//rss.bloople.net/?url=https%3A%2F%2Fwww.dogingtonpost.com%2Ftopics%2Ftraining%2Ffeed%2F&detail=100&limit=<?php echo RSS_ITEMS_TO_DISPLAY ?>&showicon=true&type=js"></script>
     </div>
+    <div class="clearfix"></div>
 </section>
