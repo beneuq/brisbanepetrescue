9d55bb0bf27ed29a93ba3111a9c0b3d6429da5bb
rosario-s <rosario_spata@outlook.com>
2021-10-11 12:55:32 +1000

    Update mypets.php

--- mypets.php
+++ mypets.php
@@ -101,34 +101,38 @@
 
             <div class="task-set3"> 
                 <h2 class="tasks-txt">Recommendations</h2>
-                <table class="tasks">
-                        <tr>
-                            <th>Recommended Food</th>
-                        </tr>
-                        <tr>
-                            <td>EXAMPLE</td>
-                        </tr>
-                </table>
+                <div class="tasks-table">
+                    <table class="tasks">
+                            <tr>
+                                <th>Recommended Food</th>
+                            </tr>
+                            <tr>
+                                <td>EXAMPLE</td>
+                            </tr>
+                    </table>
+                </div>
            
                 <h2 class="tasks-txt">In your area</h2>
-                <table class="tasks" id="nearby-places">
-                    <!-- Load Google Maps Places API Library (if enabled) -->
-                    <div id="map"></div>
-                    <?php
-                        if (USE_GOOGLE_MAPS_API) {
-                            echo "
-                            <script async src='https://maps.googleapis.com/maps/api/js?key=".GOOGLE_MAPS_API_KEY."&libraries=places'></script>
-                            <script src='js/pet-rescue-google-maps-api.js'></script>
-                            ";
-                        } else {
-                            echo "<p style='background-color:red'>Results are not being displayed to save our free API credits. <br>Enable USE_GOOGLE_MAPS_API in constants.php to test </p>";
-                        }
-                    ?>
-                    <tr id="vet-clinics"><th>Veterinary Clinics</th><th>Location</th><th>Rating</th></tr>
-                    <tr id="dog-parks"><th>Dog Parks</th><th>Location</th><th>Rating</th></tr>
-                    <tr><th>Puppy Preschools / Obedience Training</th><th>Location</th><th>Rating</th></tr>
-                    <tr><th>Dog Groomers</th><th>Location</th><th>Rating</th></tr>
-                </table>
+                <div class="tasks-table">
+                    <table class="tasks" id="nearby-places">
+                        <!-- Load Google Maps Places API Library (if enabled) -->
+                        <div id="map"></div>
+                        <?php
+                            if (USE_GOOGLE_MAPS_API) {
+                                echo "
+                                <script async src='https://maps.googleapis.com/maps/api/js?key=".GOOGLE_MAPS_API_KEY."&libraries=places'></script>
+                                <script src='js/pet-rescue-google-maps-api.js'></script>
+                                ";
+                            } else {
+                                echo "<p style='background-color:red'>Results are not being displayed to save our free API credits. <br>Enable USE_GOOGLE_MAPS_API in constants.php to test </p>";
+                            }
+                        ?>
+                        <tr id="vet-clinics"><th>Veterinary Clinics</th><th>Location</th><th>Rating</th></tr>
+                        <tr id="dog-parks"><th>Dog Parks</th><th>Location</th><th>Rating</th></tr>
+                        <tr><th>Puppy Preschools / Obedience Training</th><th>Location</th><th>Rating</th></tr>
+                        <tr><th>Dog Groomers</th><th>Location</th><th>Rating</th></tr>
+                    </table>
+                </div>
             </div>
 
             <div class="clearfix"></div>
