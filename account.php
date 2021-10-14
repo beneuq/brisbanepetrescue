a1ae30c1bd34191999826702c968b738fc5cc82a
Sean <smarshallean@gmail.com>
2021-10-14 22:41:41 +1000

    minor style updates to help page hero

--- account.php
+++ account.php
@@ -24,36 +24,32 @@
         <!-- Hero Section -->
         <section id="account-page-hero" class="hero-section pos-relative dimmer-sm">
             <div class="flex container">
-                <div class="flex f-col hero-content hero-content-full-width bg-img">
+                <div class="flex f-col hero-content bg-img">
                     <h1 class="pad-top-2 pad-bottom-1">My Account</h1>
                         <?php
                         if (user_owns_dogs()) {
                             ?>
                             <h2 class="pad-bottom-1">Welcome back, <?php echo $firstname ?>!</h2>
                             <p class="pad-bottom-2">Check up on your pet care reminders, find a new companion, or check out some articles below.</p>
-                            <div class="btn-group pad-bottom-2">
+                            <div class="btn-group margin-bottom-2">
                                 <a href="/mypets.php" class="margin-right-1 hero-btn">My pets</a>
                                 <a href="/dogs.php" class="margin-right-1 hero-btn hero-btn-alt">Explore dogs</a>
                                 <a href="/favourites.php" class="margin-right-1 hero-btn hero-btn-alt">View favourites</a>
+                            </div>
                             <?php
                         } else {
                             ?>
-                            <h2 class="pad-bottom-1">Welcome, <?php echo $firstname ?>!</h2>
-                            <p>Time to get the ball rolling on your pet adoption?</p>
-                            <p>We recommend starting with a personality quiz to find a breed that matches your lifestyle.</p>
-                            <p class="pad-bottom-2">You can also save breeds to your favorites, or keep a shortlist of dogs you can come back to at any time! </p>
-                            <div class="btn-group pad-bottom-2">
+                            <h2 class="margin-bottom-1">Welcome, <?php echo $firstname ?>!</h2>
+                            <p>Time to get the ball rolling on your pet adoption? We recommend starting with a personality quiz to find a breed that matches your lifestyle. You can also save breeds to your favorites, or keep a shortlist of dogs you can come back to at any time! </p>
+                            <div class="btn-group margin-top-2 pad-bottom-2">
                                 <a href="/quiz.php" class="margin-right-1 hero-btn">Personality Quiz</a>
                                 <a href="/dogs.php" class="margin-right-1 hero-btn hero-btn-alt">Explore dogs</a>
-                                <a href="/favourites.php" class="margin-right-1 hero-btn hero-btn-alt">View favourites</a>
+                            </div>
                             <?php
                         }
                         ?>
-                    </div>
-                    <!-- <div class="pad-bottom-2">
-                        <a href="logout.php" class="hero-btn hero-btn-alt">Logout</a>
-                    </div> -->
                     <div class="pad-bottom-2">
+                        <a href="/favourites.php" class="margin-right-1 hero-btn hero-btn-alt">View favourites</a>
                         <a href="logout.php"  class="margin-right-1 hero-btn">Log out</a>
                     </div>
                 </div>
