35c6cd7888aef4b0be2144eb9e049491e2440361
rosario-s <rosario_spata@outlook.com>
2021-10-17 22:24:33 +1000

    Update favourites.php

--- favourites.php
+++ favourites.php
@@ -75,7 +75,8 @@
                                 <td style='width:25%;'><?php echo $entry['intelligence_desc'];?></td>
                                 <td class='desktop-only' class='text-center' style='width:10%;'><?php echo str_repeat(EMOJI_DOLLAR,$entry['lifetime_cost_class']);?></td>
                                 <td class='desktop-only' class='text-left' style='width:10%;'><?php echo str_repeat(EMOJI_STAR,$entry['popularity_class']);?></td>
-                                <td style='width:16%;'> <img src='images/icons/dog_size_<?php echo $entry['size_class'];?>' alt='dog size chart' width='50%'> </td>
+                                <td class='desktop-only' style='width:16%;'> <img src='images/icons/dog_size_<?php echo $entry['size_class'];?>' alt='dog size chart' width='50%'> </td>
+                                <td class='mobile-only' style='width:90%;'> <img src='images/icons/dog_size_<?php echo $entry['size_class'];?>' alt='dog size chart' width='50%'> </td>
                                 <td style='width:5%;'>
                                     <form method='POST' action='/form_submissions/favourite_breed.php'>
                                         <button type='submit' name='breed_id' value='<?php echo $entry['breed_id'];?>'>
@@ -132,7 +133,7 @@
                                 <td class='desktop-only' style='width:10%;'><?php echo $entry['age'];?> years</td>
                                 <td class='desktop-only' style='width:5%;'><img src='/images/icons/<?php echo $entry['gender'];?>.png' alt='dog image' width='20%' class="zoom-on-hover"></td>
                                 <td style='width:20%;'><?php echo $entry['Shelter'];?></td>
-                                <td style='width:15%;'><img src='<?php echo SITEURL.$entry['path'];?>' alt='dog image' width='90%'></td>
+                                <td style='width:15%;'><img src='<?php echo SITEURL.$entry['path'];?>' alt='dog image' width='33%'></td>
                                 <td style='width:5%;'>
                                     <form method='POST' action='/form_submissions/favourite_dog.php'>
                                         <button type='submit' name='dog_id' value='<?php echo $entry['dog_id'];?>'>
