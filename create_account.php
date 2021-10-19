130ce7541cb257560a400ab81464f29950aceab1
SailorCosmos <s.guarrera@uqconnect.edu.au>
2021-10-19 15:07:04 +1000

    username and email error messages

--- create_account.php
+++ create_account.php
@@ -22,31 +22,77 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     // TODO Validate all user inputs!
     // First check if any fields are empty
     if(isset($first_name) && !empty($first_name) AND isset($last_name) && !empty($last_name) AND isset($email) && !empty($email) AND 
-    isset($dob) && !empty($dob) AND isset($username) && !empty($username) AND isset($password) && !empty($password)){
+    isset($dob) && !empty($dob) AND isset($username) && !empty($username) AND isset($password) && !empty($password)) {
         // No fields were empty so we can begin to check them
         
         // Check the email address is valid
-        // if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        alert_box("email goodo");
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // alert_box("email goodo");
 
-        //     // Let's also check if the user is already in the database
-        //     $userSQL = "SELECT username FROM users WHERE username=(username) VALUES (?)";
-        //     if($userQuery = mysqli_prepare($conn, $userSQL)) {
-        //         mysqli_stmt_bind_param($userSQL, "s", $username);
-                
-        //         if(mysqli_stmt_execute($userQuery) != 0) {
-        //             // Send user to login page now
-        //             alert_box("user exists");
-        //         } else {
-        //             alert_box("Does not exist");
-        //         }
-        //         mysqli_stmt_close($userQuery);
-        //     }
+            // Let's also check if the user is already in the database
+            $sql = "SELECT user_id, first_name, password FROM users WHERE username LIKE ?";
+            
+            // Preparing the SQL query
+            if($query = mysqli_prepare($conn, $sql)) {
+                mysqli_stmt_bind_param($query, "s", $username);
+
+                // Execute the query and store result, then check if password is correct
+                if(mysqli_stmt_execute($query)) {
+                    mysqli_stmt_store_result($query);
+
+                    // A matching username was found
+                    if(mysqli_stmt_num_rows($query)) {
+                        $userError = 'Username is already taken. Please enter another username.';
+                    } else {
+                        // Insert the user into the database
+                        $sql = "INSERT INTO users (username, first_name, last_name, dob, email, password) VALUES (?, ?, ?, ?, ?, ?)";
+
+                        if($query = mysqli_prepare($conn, $sql)) {
+                            mysqli_stmt_bind_param($query, "ssssss", $username, $first_name, $last_name, $dob, $email, $password);
+
+                            // TODO Hash the password
+                            //$password = password_hash($password, PASSWORD_DEFAULT);
+
+                            if(mysqli_stmt_execute($query)) {
+                                // Send user to login page now
+                                alert_box("Thanks for signing up, $first_name!");
+                                header("Location: /login.php");
+                            } else {
+                                alert_box("There was an error creating account. Please try again later.");
+                            }
+                            
+                            mysqli_stmt_close($query);
+                        }
+                    }
+                }
+            }
+
+            mysqli_stmt_close($query);
+                        
                 
 
             
         } else {
-            alert_box("email poopo");
+            // Set the email error
+            $emailError = 'The email you have entered is invalid. Please try again with a valid email';
+
+            // Let's also check if the user is already in the database
+            $sql = "SELECT user_id, first_name, password FROM users WHERE username LIKE ?";
+            
+            // Preparing the SQL query
+            if($query = mysqli_prepare($conn, $sql)) {
+                mysqli_stmt_bind_param($query, "s", $username);
+
+                // Execute the query and store result, then check if password is correct
+                if(mysqli_stmt_execute($query)) {
+                    mysqli_stmt_store_result($query);
+
+                    // A matching username was found
+                    if(mysqli_stmt_num_rows($query)) {
+                        $userError = 'Username is already taken. Please enter another username.';
+                    }
+                }
+            }
         }
         
         
@@ -69,9 +115,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         //     mysqli_stmt_close($query);
         // }
    // }
-
+    }
     
-}
+}    
+
 ?>
 
 <!DOCTYPE html>
@@ -84,6 +131,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- import generic head section -->
     <?php include('partials/head.php'); ?>
 </head>
+
+
+
 <div class="underneath-nav"></div>
         <!-- import menu -->
         <?php include('partials/menu.php'); ?>
@@ -108,12 +158,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="text" id="email" name="email" value="<?php echo $email;?>">
+                <?php 
+                    // Check if the email is an error and print the message
+                    if(isset($emailError)){
+                        echo '<div class="statusmsg">'.$emailError.'</div>';
+                    } 
+                ?>
                 </div>
                 <div class="form-group">
                     <label for="username">Username</label>
                     <input type="text" id="username" name="username" value="<?php echo $username;?>">
                 </div>
-
+                <?php 
+                    // Check if the username is an error and print the message
+                    if(isset($userError)){
+                        echo '<div class="statusmsg">'.$userError.'</div>';
+                    } 
+                ?>
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input type="password" id="password" name="password" value="<?php echo $password;?>">
