5570fc97c52b655dcf962af99f8bbd87cd08a24f
Benjamin Estrade <benegraphicdesign@gmail.com>
2021-10-23 09:59:44 +1000

    Updated commenting in filter-menu

--- partials/filter-menu.php
+++ partials/filter-menu.php
@@ -1,13 +1,18 @@
-<!-- Need to implement -->
 <html>
 
 <body>
     <?php
+    // adding default values
     $whereFilters = "";
     $orderFilter = "Breed";
     ?>
 
     <?php
+    /**
+     * Allows printing to console of nested arrays (2 levels) to console
+     * 
+     * $data: The variable, array or nested array you want to print
+     */
     function debug_to_console($data)
     {
         echo "<script>";
@@ -26,11 +31,19 @@
         }
         echo "</script>";
     }
-    debug_to_console($_GET);
     ?>
 
-    <!-- Create array with filter variables only -->
+
     <?php
+    /**
+     * Removes page variables that are not used for filtering or sorting
+     * 
+     * $filterTable: The name of the table in the db to be used for filtering
+     * $pageVars: The list of variables you want to 'clean'
+     * $conn: An active connection to the db
+     * 
+     * Returns: pageVars with all keys that don't match filters or sorts removed
+     */
     function filterVarList(string $filterTable, array $pageVars, $conn)
     {
         $res = mysqli_query($conn, "SELECT field_name FROM $filterTable WHERE filter_by = 1 ORDER BY filter_order");
@@ -47,8 +60,18 @@
     }
     ?>
 
-    <!-- create function for return link to current page -->
+
     <?php
+    /**
+     * Creates a page link with the existing variables +/- any variables that should be added or removed
+     * 
+     * $page: The page url without any variables
+     * $pageVars: The variables that where passed to the page
+     * $drop: If true, keys in the $filtersToModify list will be removed from the $pageVars, 
+     *      if false, value pairs in $filtersToModify replace or are added to page variables added to the url
+     * 
+     * Return: The url string with the variables that should be passed
+     */
     function createLink(string $page, array $pageVars = [], bool $drop = false, array $filtersToModify = [])
     {
         foreach ($filtersToModify as $key => $value) {
@@ -66,8 +89,17 @@
     }
     ?>
 
-    <!-- Function for creating additional where clauses -->
+
     <?php
+    /**
+     * Creates a string that contains the where clauses that would filter the results based on the filters passed
+     * 
+     * $filters: The value pairs the query should be filtered by
+     * $filterTable: The table containing the filter information, strings must be treated differently to numbers
+     * $conn: An active connection to the db
+     * 
+     * Returns: A string containing the where clauses that would filter the results based on the filters passed
+     */
     function createWhereFilters(array $filters, $filterTable, $conn)
     {
         //if empty return empty string
@@ -81,9 +113,14 @@
         while ($row = mysqli_fetch_assoc($res)) {
             $isString[$row['field_name']] = $row['is_string'];
         }
+
+        // creates where clause string
+        // starts with 'AND' and is encapsulated so it works correctly with any other filters already added to the query
         $whereFilters = "AND (";
         foreach ($filters as $filter => $value) {
+            // if string the values must be encapsulated in ""
             if ($isString[$filter]) {
+                // if value is an array use the 'IN' clause so any matching value is ok
                 if (is_array($value)) {
                     $inFilter = '';
                     foreach ($value as $subVal) {
@@ -91,8 +128,11 @@
                     }
                     $whereFilters .= " $filter IN (" . rtrim($inFilter, ",") . ") AND ";
                 } else {
+                    // used '=' when a single value is passed
                     $whereFilters .= " $filter = \"$value\" AND ";
                 }
+                // if value is numeric and encapsulated in "" it would break the query
+                // does the same as above without the ""
             } else {
                 if (is_array($value)) {
                     $inFilter = '';
@@ -105,6 +145,8 @@
                 }
             }
         }
+
+        // 'AND' is added after each filter and thus there is an extra at the end
         $whereFilters = rtrim($whereFilters, "AND ");
         $whereFilters .= ") ";
         return $whereFilters;
@@ -113,8 +155,15 @@
 
 
 
-    <!-- Function for creating order clause -->
     <?php
+    /**
+     * Creates the sort by string to be added to the main sql query
+     * 
+     * $pageVars: The variables to look for a sort by value in and if there is an ascending key
+     * $filterTable: The name of the db table to confirm that variable requested is a valid sort filter
+     * 
+     * Returns: the sort by string name and ' desc' when set to descending order
+     */
     function createOrderBy(array $pageVars, string $filterTable, $conn)
     {
         if (!empty($pageVars) and (isset($pageVars['sortby']) or isset($pageVars['ascending']))) {
@@ -142,8 +191,12 @@
     }
     ?>
 
-    <!-- Creating filters for current query -->
+
     <?php
+    /**
+     * Uses the functions above to generate values passed to the sql query for filtering and sorting
+     * the results being displayed
+     */
     $filters = filterVarList($filterTable, $_GET, $conn);
     $orderFilter = createOrderBy($_GET, $filterTable, $conn);
     // where filter used for displaying results
@@ -152,10 +205,24 @@
     $whereFiltersForFilter = $whereFilters;
     ?>
 
-    <!-- Rename filters that are classes or bools -->
+
     <?php
+    /**
+     * Returns a user readible string equavilent of the field value depending on
+     * if it is an array, is a class value or boolean value
+     * 
+     * $field_name: numeric, string or array,
+     *      if array: 'Multiple' is returneds
+     *      if string: $field_name is returned
+     *      if numeric: is mapped to a string based on $is_class and $is_bool
+     * $is_class: if $field_name is a class value it is mapped to the respective class string
+     * $is_bool: if $field_name is a boolean value it is mapped to the respective bool string
+     * 
+     * Returns: The relevant user readible string for the $field_name
+     */
     function getFieldName($field_name, $is_class, $is_bool)
     {
+        // if an array of values just return 'Multiple'
         if (is_array($field_name)) {
             return "Multiple";
         }
@@ -167,21 +234,29 @@
             4 => "High",
             5 => "Very High"
         );
+        // if class map to the relevant class string
         if ($is_class == 1) {
             return $class_names[$field_name];
         }
+        // if boolean map to the relevant bool string
         if ($is_bool == 1) {
             if ($field_name == 1) {
                 return "Yes";
             }
             return "No";
         }
+        // otherwise return value passed
         return $field_name;
     }
     ?>
 
     <!-- Add the current filters to be selected and removed -->
     <?php
+    /**
+     * Generates a html table of the existing filter pairs
+     * which when each filter is clicked it removes it as a filter
+     * and refreshes the page
+     */
     if (!empty($filters)) {
         $existing = "<table class='filter-current'>
 	<colgroup span=\"2\"></colgroup>
@@ -205,11 +280,18 @@
     }
     ?>
 
-    <!-- Add all filters to div -->
+
     <?php
+    /**
+     * Generates the each filter that is available as a table
+     * The possible values for the filter and the number of results that would be displayed are added to a
+     * second table that when selected adds them as a filter
+     */
+    // getting filter information from database
     $res = mysqli_query($conn, "SELECT field_name, display_name, class_field, bool_field 
     FROM $filterTable WHERE filter_by = 1 ORDER BY filter_order");
     $newFilters = "";
+    // generating a table for each filter available
     while ($row = mysqli_fetch_assoc($res)) {
         $res2 = mysqli_query($conn, "SELECT " . $row['field_name'] .
             " as field_value, COUNT(*) as field_count FROM $table " . $whereFiltersForFilter .
@@ -221,6 +303,8 @@
         </tr></table>
         <table class='filter-results'>
         <colgroup span=\"2\"></colgroup>";
+            // generating a table for each of the available options with a filter and the number
+            // of results that would be displayed if the filter was selected
             $newFilters .= "<tr class='filter-3'><th scope=\"col\"><a href=\"";
             $newFilters .= createLink($page, $_GET, 0, array($row['field_name'] => $row2['field_value']));
             $newFilters .= "\">" . getFieldName($row2['field_value'], $row['class_field'], $row['bool_field']) .
@@ -234,8 +318,10 @@
             $newFilters .= "</table>";
         }
     }
+    // adds filter tables to html
     echo $newFilters;
     ?>
+    <!-- Script for displaying filters -->
     <script>
         var elements = document.getElementsByClassName("filter-1");
         for (var i = 0; i < elements.length; i++) {
