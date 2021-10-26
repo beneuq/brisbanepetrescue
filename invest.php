bf4e98c72f4b0b3227553aff55373086b1446bf9
Matt Powell <matt@mpowell.tech>
2021-10-26 22:16:14 +1000

    Added a basic invest page based on contact us template

---
+++ invest.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- import generic head section -->
+    <?php
+    $page_title = 'Invest';
+    include('partials/head.php');
+    $active_contact = 'active';
+    ?>
+</head>
+
+<body>
+    <div class="underneath-nav"></div>
+    <!-- import menu -->
+    <?php include('partials/menu.php'); ?>
+
+    <!-- contact form -->
+    <div id="contact-bg" class="container wrapper">
+        <form id="contact" onsubmit="contactSubmit()">
+            <fieldset class="input-card">
+                <legend>
+                    Contact Us to Invest
+                </legend>
+                <div id="contact-inf">
+                    <div>
+                        <label>Name:</label>
+                        <input type="text" placeholder="First and last names" required>
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <input type="email" placeholder="Enter a valid email address" required>
+                    </div>
+                    <div>
+                        <label>Message:</label>
+                        <textarea placeholder="Write about your intention to donate and we'll be in touch!" required></textarea>
+                    </div>
+                    <button class="submit-btn" type="submit" value="Submit">Submit</button>
+                </div>
+
+            </fieldset>
+
+        </form>
+    </div>
+    <!-- FOOTER -->
+    <?php include('partials/footer.php'); ?>
+
+    <!-- Script for handle contact form submissions -->
+    <script src="js/submit.js"></script>
+</body>
+
+</html>
